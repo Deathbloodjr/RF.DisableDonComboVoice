764702b5e82cd968027f6b550e3f9b1d24575e9f
Deathblood <deathbloodjr@gmail.com>
2024-09-03 20:38:06 -0500

    Initial commit

---
+++ DisableDonComboVoice/Plugins/DisableDonComboVoicePatch.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisableDonComboVoice.Plugins
+{
+    internal class DisableDonComboVoicePatch
+    {
+        [HarmonyPatch(typeof(EnsoSound))]
+        [HarmonyPatch(nameof(EnsoSound.KeyOnGameDonMekaVoice))]
+        [HarmonyPatch(MethodType.Normal)]
+        [HarmonyPrefix]
+        public static bool EnsoSound_KeyOnGameDonMekaVoice_Prefix(EnsoSound __instance, string toneName)
+        {
+            Plugin.Log.LogInfo("");
+            Plugin.Log.LogInfo("EnsoSound_KeyOnGameDonMekaVoice_Prefix");
+
+            // removes the 50combo, 100combo, xxcombo voice
+            // doesn't remove fullcombo voice
+            if (toneName.Contains("0combo"))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# RF.DisableDonComboVoice
+ A Rhythm Festival mod to disable Don-Chan's voice when hitting combo milestones.
